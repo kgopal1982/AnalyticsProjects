678a67e5592755a0a8ea4e773c99ac5221af05d8
KrishnaGoswami <kgopal_1982@yahoo.com>
2019-03-01 11:26:04 +0530

    Add files via upload

--- CrimeProject/WorkInProgress/CrimeTwitterAnalysis.py
+++ CrimeProject/WorkInProgress/CrimeTwitterAnalysis.py
@@ -4,13 +4,16 @@
 #https://galeascience.wordpress.com/2016/03/18/collecting-twitter-data-with-python/
 #https://github.com/agalea91/twitter_search/blob/master/twitter_search.py
 
-from tweepy.streaming import StreamListener
-from tweepy import Stream
+#from tweepy.streaming import StreamListener
+#from tweepy import Stream
 import tweepy
 from tweepy import OAuthHandler
 import datetime as dt
 import time
 import json
+import pandas as pd
+import os
+from datetime import datetime
 
 
 def load_api():
@@ -54,38 +57,81 @@ def write_tweets(tweets, filename):
             json.dump(tweet._json, f)
             f.write('\n')
 
-#class StdOutListener(StreamListener):
-#    """ A listener handles tweets that are received from the stream.
-#    This is a basic listener that just prints received tweets to stdout.
-#    """
-#    def on_status(self, status):
-#        print(status.text)
-#        return True
-#
-#    def on_error(self, status_code):
-#        if status_code == 420:
-#            return False
-        
-def main():
-    ''' search variables: '''
-#    search_phrases = ['Robbery', 'Kidnap', 
-#                     'Shooting', 'Arrest',
-#                     'Homicide', 'Narcotics',
-#                     'Marijuana']
-    search_phrases = ['Marijuana']
-    #time_limit = 1.5        # runtime limit in hours
-    max_tweets = 500        # number of tweets per search (will be iterated over) - maximum is 100
-    Chicago = '41.881832, -87.623177, 320km'
-    #Chicago = '39.8,-95.583068847656,2500km'
-    json_file = "C:\\datasets\\crimedata\\crimes_tweets.json"
+            
+def create_directory():
+    #create the directoty where we will store the json
+    today = datetime.now()
+    base_dir = "C:/DataSets/crimedata/tweetAnalysis"
+
+    full_dir = os.path.join(base_dir, today.strftime('%Y{0}%m{0}%d').format(os.path.sep))
     
-    # authorize and load the twitter API
-    api = load_api()
-     
-    # collect tweets
-    tweets = tweet_search(api, search_phrases, max_tweets,geocode=Chicago)
-    #tweets = tweet_search(api, search_phrases, max_tweets)
-    write_tweets(tweets, json_file)
+    if not os.path.exists(full_dir):
+        os.makedirs(full_dir)
+    
+    return full_dir
+
+    
+def store_process_csv(json_file):
+    #load the json file   
+    df_orig = pd.read_json(json_file, lines=True)
+    #save into csv
+    full_dir = create_directory()
+    df_orig.to_csv(os.path.join(full_dir,r'original_crimes_tweets.csv'))
+    #df_orig.to_csv("C:\\DataSets\\crimedata\\tweetAnalysis\\2019\\03\\01\\original_crimes_tweets.csv")
+    #print(df_orig.head())
+    df = df_orig.copy(deep = True)
+
+    #keep the columns which are of interest
+    df = df[['created_at','id','id_str','lang','retweet_count','retweeted','source','text','user']]
+    
+    #check for duplicates
+    df.duplicated('id')
+    
+    #it shows there are duplicates
+    #drop the duplicates
+    df.drop_duplicates(subset = "id", keep = 'first', inplace = True)
+    df_filtered = df[df['lang']=="en"]
+    #save into csv
+    df_filtered.to_csv(os.path.join(full_dir,r'modified_crimes_tweets.csv'))
+    #df.to_csv("C:\\DataSets\\crimedata\\tweetAnalysis\\2019\\03\\01\\modified_crimes_tweets.csv")
+
+def main():
+    #create the folders based on date
+    full_dir = create_directory()
+    
+    #create the json file
+    json_file = os.path.join(full_dir,'crime_tweets.json')
+
+    f = open(json_file,'w')
+
+    ''' search variables: '''
+    search_params = [['Robbery'], ['Kidnap'], ['Shooting'], ['Arrest'],['Homicide'], ['Narcotics'],['Marijuana']]
+    
+    #loop through each search term and dump the tweets as json
+    for i in range(0, len(search_params)):
+        search_phrases = search_params[i]
+        #print("Generating tweet for:"+ search_phrases)
+    
+        #time_limit = 1.5        # runtime limit in hours
+        max_tweets = 500        # number of tweets per search (will be iterated over) - maximum is 100
+        Chicago = '41.881832, -87.623177, 320km'
+        #Chicago = '39.8,-95.583068847656,2500km'
+        #json_file = "C:\\datasets\\crimedata\\crimes_tweets_26thFeb.json"
+        
+        # authorize and load the twitter API
+        api = load_api()
+         
+        # collect tweets
+        tweets = tweet_search(api, search_phrases, max_tweets,geocode=Chicago)
+        #tweets = tweet_search(api, search_phrases, max_tweets)
+        write_tweets(tweets, json_file)
+    
+    #store the json file into csv and process
+    store_process_csv(json_file)
+    #close the file handle
+    f.close()
+    
     
 if __name__ == "__main__":
     main()
+    print("Process got executed")
