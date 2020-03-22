f31037372bbc6fdfa400a048e31bbf4a94f87e7d
kgopal1982 <32187866+kgopal1982@users.noreply.github.com>
2020-03-22 08:52:58 +0530

    Add files via upload

---
+++ newspapaperScrapping/english_elpais_com/extract_html.py
@@ -0,0 +1,50 @@
+#https://realpython.com/python-web-scraping-practical-introduction/
+#https://towardsdatascience.com/web-scraping-news-articles-in-python-9dd605799558
+
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+    
+raw_html = simple_get('https://english.elpais.com/')
+html_file= open("C:\\Users\\ekrigos\\Desktop\\DataS\\Projects\\web_scrapping\\raw_file_elpais.html","wb")
+html_file.write(raw_html)
+html_file.close()
+
+
